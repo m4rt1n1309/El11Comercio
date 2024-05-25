55c377b0c9b27e0a5ccc2bfb3c35a0d28a223e11
martin <casaresrm@gmail.com>
2024-05-25 15:14:51 -0300

    seccion services

--- src/constants/index.js
+++ src/constants/index.js
@@ -64,3 +64,39 @@ export const products = [
     price: "$230.20",
   },
 ];
+
+export const services = [
+  {
+    imgURL: truckFast,
+    label: "Free shipping",
+    subtext: "Enjoy seamless shopping with our complimentary shipping service.",
+  },
+  {
+    imgURL: shieldTick,
+    label: "Secure Payment",
+    subtext:
+      "Experience worry-free transactions with our secure payment options.",
+  },
+  {
+    imgURL: support,
+    label: "Love to help you",
+    subtext: "Our dedicated team is here to assist you every step of the way.",
+  },
+];
+
+export const reviews = [
+  {
+    imgURL: customer1,
+    customerName: "Morich Brown",
+    rating: 4.5,
+    feedback:
+      "The attention to detail and the quality of the product exceeded my expectations. Highly recommended!",
+  },
+  {
+    imgURL: customer2,
+    customerName: "Lota Mongeskar",
+    rating: 4.5,
+    feedback:
+      "The product not only met but exceeded my expectations. I'll definitely be a returning customer!",
+  },
+];
