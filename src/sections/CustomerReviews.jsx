638e06bcea698d8a2f998cc6570ed105a3a912bc
martin <casaresrm@gmail.com>
2024-06-05 20:37:34 -0300

    seccion customer reviews

---
+++ src/sections/CustomerReviews.jsx
@@ -0,0 +1,30 @@
+import { reviews } from "../constants";
+import ReviewCard from "../components/ReviewCard";
+
+function CustomerReviews() {
+  return (
+    <section className="max-container">
+      <h3 className="font-palanquin text-center text-4xl font-bold">
+        What Our
+        <span className="text-coral-red"> Customers</span> Say?
+      </h3>
+      <p className="info-text m-auto mt-4 max-w-lg text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        pariatur dicta consequatur ex? Nam omnis saepe itaque a, beatae eaque.
+      </p>
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+        {reviews.map((review) => (
+          <ReviewCard
+            key={review.customerName}
+            imgURL={review.imgURL}
+            customerName={review.customerName}
+            rating={review.rating}
+            feedback={review.feedback}
+          />
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default CustomerReviews;
